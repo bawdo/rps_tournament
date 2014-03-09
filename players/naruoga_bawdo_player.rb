3cd94317c98ce7161e5ca864aafafb6bf0ffbf2b
Keith Bawden <keith@bawdo.com>
2014-03-09 17:11:15 +0900

    Beating SequencePlayer %99.55 of the time

--- players/naruoga_bawdo_player.rb
+++ players/naruoga_bawdo_player.rb
@@ -1,13 +1,24 @@
 class NaruogaBawdoPlayer < RpsTournament::Player
 
-  @@results = [[:paper, :scissors, :lose]]
-  OPTIONS = [:rock, :paper, :scissors]
+  OPTIONS = [:paper, :rock, :scissors]
+
+  attr_accessor :results, :seq
+
+  def initialize
+    self.results = []
+    self.seq     = false
+  end
 
   def choose
-    last_result = @@results.last.last
-    last_my_choice = @@results.last[0]
-    last_opponent_choice = @@results.last[1]
-    if last_result == :lose
+
+    return OPTIONS.sample if results.empty?
+    last_result = results.last.last
+    last_my_choice = results.last[0]
+    last_opponent_choice = results.last[1]
+
+    if detect_sequence_player
+      last_opponent_choice
+    elsif last_result == :lose
       OPTIONS.sample
     elsif last_result == :win
       last_my_choice
@@ -17,17 +28,20 @@ class NaruogaBawdoPlayer < RpsTournament::Player
   end
 
   def result(my_choice, opponent_choice, result)
-    @@results << [my_choice, opponent_choice, result]
+    results << [my_choice, opponent_choice, result]
   end
 
   private
 
+  def last_six_opponent_plays
+    results.length > 5 ? results[-6,6].map {|i| i[1] } : results.map {|i| i[1] }
+  end
+
   def detect_sequence_player
-    if @@results.length > 6 &&
-      @results[-6,6] == [:paper, :rock, :scissors, :paper, :rock, :scissors]
-    else
-      false
+    if results.length % 6 == 0
+      self.seq = ( last_six_opponent_plays == [:rock, :scissors, :paper, :rock, :scissors, :paper] )
     end
+    return seq
   end
 
 end
